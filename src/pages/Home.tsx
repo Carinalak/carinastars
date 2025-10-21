19b45a1775deeb2effb88d4734897bc761fc087e
Carinalak <carina.lakosil@gmail.com>
2025-10-21 12:36:30 +0200

    Add info on news page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,11 @@
 import { BackgroundOriginal } from "../components/styled/Wrappers";
 import BackgroundJade from "../assets/images/carina/Jade_akustisk.jpg"
 import VisionLp from "../assets/images/lp/Vision_of_life.png"
+import CarinaReturn from "../assets/images/carina/carina_return.jpg"
+import LuckySoulDreaming from "../assets/images/carina/LuckySoulDreaming.jpg"
+import AmISick from "../assets/images/carina/AmISick.jpg"
 import styled from "styled-components";
-import { BLACK, BREAKPOINT_BIGGER_DESKTOP, BREAKPOINT_DESKTOP, BREAKPOINT_TABLET, WHITE } from "../components/styled/Variables";
+import { BLACK, BREAKPOINT_BIGGER_DESKTOP, BREAKPOINT_DESKTOP, BREAKPOINT_TABLET, LIGHTERPURPLE, LIGHTPURPLE } from "../components/styled/Variables";
 import { H3Black } from "../components/styled/Fonts";
 
 export const NewsLpImage = styled.img `
@@ -11,9 +14,18 @@ export const NewsLpImage = styled.img `
  //margin-top: 20px;
 
     @media screen and (min-width: ${BREAKPOINT_TABLET}) {
-       width: 200px;
+      width: 200px;
     }
 `;
+
+export const NewsLpImageSecond = styled(NewsLpImage) `
+  display: none;
+
+    @media screen and (min-width: ${BREAKPOINT_DESKTOP}) {
+      display: block;
+    }
+`;
+
 export const BackImg = styled.div`
   width: 100vw;
   height: 100vh;
@@ -33,21 +45,26 @@ export const NewsContainer = styled.div `
   //height: 300px;
   border-radius: 5px;
   margin-top: 60px;
+  margin-bottom: 20px;
   padding: 5px;
   color: ${BLACK};
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 5px;
+  gap: 10px;
+
 
     @media screen and (min-width: ${BREAKPOINT_TABLET}) {
       flex-direction: row;
       flex-wrap: wrap;
       align-items: start;
+      gap: 5px;
     }
     @media screen and (min-width: ${BREAKPOINT_DESKTOP}) {
       margin: 0;
+      margin-bottom: 20px;
+      gap: 10px;
     }
     @media screen and (min-width: ${BREAKPOINT_BIGGER_DESKTOP}) {
       margin: 0;
@@ -58,7 +75,8 @@ export const NewsArticleFirst = styled.div `
   width: 90%;
   //background: linear-gradient(to bottom, #ffffffa8, #bcb5b5a8);
   //background-color: #ffffffa8;
-  background-color: ${WHITE};
+  //background-color: #f5f2f5a8;
+  background-color: ${LIGHTERPURPLE};
   border: 1px solid black;
   border-radius: 5px;
   display: flex;
@@ -76,17 +94,22 @@ export const NewsArticleFirst = styled.div `
 `;
 
 export const NewsArticleSecond = styled(NewsArticleFirst) `
-  background-color: #eb80eda8;
+  //background-color: #eb80eda8;
+  background-color: ${LIGHTPURPLE};
 `;
 
 export const NewsArticleThird = styled(NewsArticleFirst)`
-  background-color: #344fc8a8;
+  //background-color: #344fc8a8;
+  background-color: ${LIGHTERPURPLE};
 
     @media screen and (min-width: ${BREAKPOINT_TABLET}){
-    //width: 600px;
+    width: 600px;
+  }
+      @media screen and (min-width: ${BREAKPOINT_DESKTOP}){
     width: calc(100% - 35px);
   }
 `;
+
 export const NewsArticleTextContainer = styled.div `
   width: 95%;
   height: 80%;
@@ -112,7 +135,8 @@ export const InnerTextCenter = styled.div `
   display: flex;
   flex-direction: row;
   align-items: center;
-
+  padding-top: 10px;
+  padding-bottom: 10px;
 `;
 
 export const InnerTextLeft = styled.div `
@@ -120,8 +144,9 @@ export const InnerTextLeft = styled.div `
   flex-direction: row;
   justify-content: left;
   width: 300px;
+  padding-top: 10px;
+  padding-bottom: 10px;
     @media screen and (min-width: ${BREAKPOINT_DESKTOP}){
-
       width: 250px;
     }
 `;
@@ -135,7 +160,7 @@ return (  <>
       <NewsContainer>
 
         <NewsArticleFirst>
-          <H3Black>Nyaste skivan</H3Black>
+          <H3Black>Ny skiva</H3Black>
           <NewsArticleTextContainer>
           <InnerTextCenter><NewsLpImage src={VisionLp} /></InnerTextCenter>
           <InnerTextLeft> Senaste skivan "Vision of Life" släpptes i september 2025. 
@@ -149,8 +174,33 @@ return (  <>
           
 
 
-        <NewsArticleSecond>adsfasdf</NewsArticleSecond>
-        <NewsArticleThird>adsfasdf</NewsArticleThird>
+        <NewsArticleSecond>
+          <H3Black>Vem är Carina Stars?</H3Black>
+          <NewsArticleTextContainer>
+            <InnerTextCenter><NewsLpImage src={CarinaReturn} /></InnerTextCenter>
+            <InnerTextLeft> Carina Stars är text och musikkompositör. Hon har varit verksam sedan 90-talet 
+              och släppte sin första skiva Lucky Soul 2019. Hennes musik är främst på engelska men hon har även 
+              några svenska låtar. Bland annat den populära "Albin" som finns på första skivan. 
+              Hon är uppvuxen norr om Stockholm och var redan som liten fascinerad av musik. 1992 var hon med i 
+              hårdrocksbandet Iguanas där hon spelade komp och sologitarr, och senare blev hon singer 
+              songwriter artist.
+            </InnerTextLeft>
+            </NewsArticleTextContainer>
+        </NewsArticleSecond>
+
+        <NewsArticleThird>
+          <H3Black>Musikvideos</H3Black>
+          <NewsArticleTextContainer>
+            <InnerTextCenter><NewsLpImageSecond src={AmISick} /></InnerTextCenter>
+            <InnerTextCenter><NewsLpImage src={LuckySoulDreaming} /></InnerTextCenter>
+            <InnerTextLeft> Titta på Carina Stars musikvideos. Du hittar dem på YouTube.
+            </InnerTextLeft>
+
+            
+
+            </NewsArticleTextContainer>
+
+        </NewsArticleThird>
       </NewsContainer>
   <BackImg>
 
