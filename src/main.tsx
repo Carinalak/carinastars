842f9a50c3a6ef1ffdfb08540f2a96fa435ff046
Carinalak <carina.lakosil@gmail.com>
2025-10-26 09:36:54 +0100

    Upload on webb -configuration-test2

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { registerSW } from 'virtual:pwa-register'
 
+// Lägg till meta-taggen med versionen dynamiskt
+const meta = document.createElement('meta')
+meta.name = 'version'
+meta.content = import.meta.env.VITE_APP_VERSION
+document.head.appendChild(meta)
 
+// Registrera service worker
 registerSW({ immediate: true })
 
 
