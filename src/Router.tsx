8e1b0cb6695dc879c50ae7b75e2a3da4000c7edb
Carinalak <carina.lakosil@gmail.com>
2025-10-26 13:08:42 +0100

    Changed to BrowserRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,3 @@
-import { createHashRouter } from "react-router-dom";
 //import { Layout } from "./pages/Layout";
 //import { NotFound } from "./pages/NotFound";
 import { NotFound } from "./pages/NotFound";
@@ -7,9 +6,10 @@ import { Home } from "./pages/Home";
 import { Music } from "./pages/Music";
 import { Contact } from "./pages/Contact";
 import { SingleLpPage } from "./components/music/SingleLpPage";
+import { createBrowserRouter } from "react-router-dom";
 
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout></Layout>,
